906510197fd7a385ffb4d20a5a732e2236db0792
Ramil Safarov <ram.seferov12@gmail.com>
2025-02-26 11:25:43 +0400

    gsap animation installed

--- js/script.js
+++ js/script.js
@@ -1,47 +1,22 @@
 'use strict';
-const slides = document.querySelectorAll('.slide');
-const titles = document.querySelectorAll('.top-title');
-let currentIndex = 0;
-let totalSlides = slides.length;
 
-// Initially set the first slide to active
-slides[currentIndex].classList.add('active');
-titles[currentIndex].classList.add('active');
-// Buttons for navigation
-document.querySelector('.prev').addEventListener('click', () => changeSlide(-1));
-document.querySelector('.next').addEventListener('click', () => changeSlide(1));
-
-// Function to change the slide
-function changeSlide(direction) {
-  // Hide the current slide and title
-  slides[currentIndex].classList.remove('active');
-  titles[currentIndex].classList.remove('active');
-  // Calculate the next slide index with infinite scrolling
-  currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-
-  // Show the new slide and title
-  setTimeout(() => {
-    titles[currentIndex].classList.add('active');
-    slides[currentIndex].classList.add('active');
-  }, 300); // Delay to match the CSS transition duration
-}
-
-// let autoplay = setInterval(() => changeSlide(1), 5000); // Auto-slide every 5 seconds
-
-// // Stop autoplay when mouse is hovering over the slides
-// document.querySelector('.slides').addEventListener('mouseover', () => clearInterval(autoplay));
-// document.querySelector('.slides').addEventListener('mouseout', () => {
-//     autoplay = setInterval(() => changeSlide(1), 5000);
-//   });
-
-
-
-
-
-
-
-
+// Preloader
 document.addEventListener('DOMContentLoaded', function () {
+  new Splide('#header-slider', {
+    type: 'fade',
+    rewind: true,
+    perPage: 1,
+    perMove: 1,
+    arrows: true,
+    pagination:false,
+    easing: 'ease-in-out',
+    transition: 'fade',
+    autoplay: true,
+    speed:900,
+    interval: 3000,
+    pauseOnHover: true,
+
+  }).mount();
   new Splide('#image-slider', {
     type: 'loop',
     perPage: 3,
@@ -66,9 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
     paginationDirection: 'ltr',
     paginationPosition: 'bottom',
   }).mount();
-});
-
-document.addEventListener('DOMContentLoaded', function () {
   new Splide('#partner-slider', {
     type: 'loop',
     perPage: 3,
@@ -90,14 +62,16 @@ document.addEventListener('DOMContentLoaded', function () {
   }).mount();
 });
 
+
+
 let closeBtn = document.querySelector('.menu-close');
 let openBtn = document.querySelector('.mobile-menu');
 
 openBtn.addEventListener('click', () => {
   openMobileNav();
 });
-closeBtn.addEventListener('click', closeMobileNav);
-document.querySelector('.mobile-navbar-layer').addEventListener('click', closeMobileNav);
+// closeBtn.addEventListener('click', closeMobileNav);
+document.querySelector('.mobile-navbar-overlay').addEventListener('click', closeMobileNav);
 
 function openMobileNav() {
   document.querySelector('.mobile-navbar').style.width = '47.5%';
@@ -109,13 +83,3 @@ function closeMobileNav() {
   document.querySelector('.mobile-navbar-overlay').classList.remove('body-overlay');
 }
 
-function onScroll_navBg() {
-
-  if (window.scrollY >= 60) {
-    document.querySelector('.header-overlay').style.height = '100%';
-  } else {
-    document.querySelector('.header-overlay').style.height = '0%';
-  }
-}
-
-document.addEventListener('scroll', onScroll_navBg);
